209613eb773e9076c347de09f756013fefa5f155
Alexandr <sasha_stepanov94@mail.ru>
2024-06-18 12:22:49 +0300

    Second ready

---
+++ Practice-16.6.2_fraction_number_stitcher.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+std::string beforDotAfterDot() {
+  std::string str = "";
+  int value;
+  int n = 0;
+
+  do {
+    if (n == 0) {
+      std::cout << "Input number before dot: ";
+    } else {
+      std::cout << "Input number after dot: ";
+    }
+    std::cin >> value;
+
+    if (value < 0) {
+      std::cerr << "Error. The number after the dot must be positive\n";
+      continue;
+    }
+
+    str += std::to_string(value);
+    if (n == 0) {
+      str += '.';
+    }
+    
+    n++;
+  } while (n < 2);
+
+  return str;
+}
+
+int main() {
+  double number = stod(beforDotAfterDot());
+
+  std::cout << "You number: " << number;
+}
