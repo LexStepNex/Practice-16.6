2010e8b6f894e5d12440dea87d56a20297338872
Alexandr <sasha_stepanov94@mail.ru>
2024-06-19 17:48:53 +0300

    Final?

--- Practice-16.6.5_Smart_house.cpp
+++ Practice-16.6.5_Smart_house.cpp
@@ -6,11 +6,11 @@ enum switches {
   LIGHT_OUTSIDE = 2,
   HEATING = 4,
   HEATING_WATER_PIPE = 8,
-  CONDITION = 16
+  CONDITIONER = 16
 };
 
 bool input_control(std::string str) {
-  {
+  {  // проверка пробелов
     int count = 0;
     for (int i = 0; i < str.size(); i++) {
       if (isspace(str[i])) {
@@ -27,12 +27,14 @@ bool input_control(std::string str) {
   buffer_str >> num_1 >> num_2 >> movement >> light;
 
   for (int i = 0; i < num_1.size(); i++) {
+    if (num_1[0] = '-') continue;
     if (!isdigit(num_1[i])) {
       std::cerr << "Enter the inside temperature as a number only\n";
       return false;
     }
   }
   for (int i = 0; i < num_2.size(); i++) {
+    if (num_2[0] = '-') continue;
     if (!isdigit(num_2[i])) {
       std::cerr << "Enter the outside temperature as a number only\n";
       return false;
@@ -50,24 +52,117 @@ bool input_control(std::string str) {
   return true;
 }
 
-void printNotes(int val) {}
+int color_temperature(int time) {
+  int temp = 5000;
+  if (time >= 16 && time <= 20) {
+    temp = 5000 + (16 - time) * 575;
+  } else if (time > 20) {
+    temp = 5000 - 4 * 575;
+  }
+  if (time == 0) temp = 5000;
+  return temp;
+}
+
+int light_inside(int switches, std::string light, bool times_Of_day, int time) {
+  if (!(switches & LIGHT_iNSIDE) && light == "on") {  // вкл свет в доме
+    switches |= LIGHT_iNSIDE;
+    std::cout << "LIGHT_iNSIDE ON\n";
+  } else if ((switches & LIGHT_iNSIDE) && light == "off") {  // выкл свет в доме
+    switches &= ~LIGHT_iNSIDE;
+    std::cout << "LIGHT_iNSIDE OFF\n";
+  }
+  if (light == "on") {
+    std::cout << "Color temperature: " << color_temperature(time) << "K\n";
+  }
+  return switches;
+}
+
+int light_outside(int switches, std::string light, bool time) {
+  if (!(switches & LIGHT_OUTSIDE) && light == "yes" &&
+      time) {  // вкл свет в саду
+    switches |= LIGHT_OUTSIDE;
+    std::cout << "LIGHT_OUTSIDE ON\n";
+  } else if ((switches & LIGHT_OUTSIDE) && !time ||
+             (switches & LIGHT_OUTSIDE) && light == "no") {  // выкл свет в саду
+    switches &= ~LIGHT_OUTSIDE;
+    std::cout << "LIGHT_OUTSIDE OFF\n";
+  }
+  return switches;
+}
+
+int heaters(int switches, int inside) {
+  if (!(switches & HEATING) && inside < 22) {  // вкл отопление труб
+    switches |= HEATING;
+    std::cout << "HEATING ON\n";
+  }
+  if ((switches & HEATING) && inside >= 25) {  // выкл отопление труб
+    switches &= ~HEATING;
+    std::cout << "HEATING OFF\n";
+  }
+  return switches;
+}
+
+int water_pipe(int switches, int outside) {
+  if (!(switches & HEATING_WATER_PIPE) && outside < 0) {  // вкл отопление труб
+    switches |= HEATING_WATER_PIPE;
+    std::cout << "HEATING WATER PIPE ON\n";
+  }
+  if ((switches & HEATING_WATER_PIPE) && outside > 5) {  // выкл отопление труб
+    switches &= ~HEATING_WATER_PIPE;
+    std::cout << "HEATING WATER PIPE OFF\n";
+  }
+  return switches;
+}
+
+int air_conditioner(int switches, int inside) {
+  if (!(switches & CONDITIONER) && inside >= 30) {  // вкл отопление труб
+    switches |= CONDITIONER;
+    std::cout << "CONDITION ON\n";
+  }
+  if ((switches & CONDITIONER) && inside < 26) {  // выкл отопление труб
+    switches &= ~CONDITIONER;
+    std::cout << "CONDITION OFF\n";
+  }
+  return switches;
+}
 
 int main() {
-  std::cout << "Calculator\n";
-  std::string indicators;
-  do {
-    std::cout
-        << "Temperature inside, temperature outside, movement, lights: \n";
-    std::getline(std::cin, indicators);
-  } while (!input_control(indicators));
+  std::cout << "Smart house\n";
 
-  int temp_inside, temp_outside;
-  std::string movement, light;
-  std::stringstream buffer_indicators(indicators);
-  buffer_indicators >> temp_inside >> temp_outside >> movement >> light;
+  int time = 0;
+  int switches_states = 0;
+  int color_temp = 5000;
 
-  std::cout << "T inside: " << temp_inside << "\n";
-  std::cout << "T outside: " << temp_outside << "\n";
-  std::cout << "Movement: " << movement << "\n";
-  std::cout << "Light: " << light << "\n";
+  for (int i = 0; i < 25; i++, time++) {
+    std::cout << "Time O'clock: " << (time < 10 ? "0" : "") << time << ":00\n";
+    
+    std::string indicators;
+
+    do {
+      std::cout
+          << "Temperature inside, temperature outside, movement, lights: \n";
+      std::getline(std::cin, indicators);
+    } while (!input_control(indicators));
+
+    int temp_inside, temp_outside;
+    std::string movement, light;
+
+    std::stringstream buffer_indicators(indicators);
+    buffer_indicators >> temp_inside >> temp_outside >> movement >> light;
+
+    bool night = time >= 16 || time < 5;
+    switches_states = light_outside(switches_states, movement, night);
+
+    switches_states = heaters(switches_states, temp_inside);
+    switches_states = water_pipe(switches_states, temp_outside);
+
+    switches_states = air_conditioner(switches_states, temp_inside);
+
+    bool evening = time >= 16 || time < 20;
+    switches_states = light_inside(switches_states, light, evening, time);
+
+    std::cout << "\n";
+
+    if (time == 23) time = -1;
+  }
 }
